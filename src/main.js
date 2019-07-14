71972a663a046de434f3934567f3c6d3e942b6e8
EcChris <42623282+EcChris@users.noreply.github.com>
2019-07-14 16:50:52 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import { heartbeat } from './helpers/heartbeat'
 Vue.config.productionTip = false
 Vue.use(abilitiesPlugin, ability)
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000' // https://stating.opengxp.com/api
+axios.defaults.baseURL = 'https://stating.opengxp.com/api'
 axios.defaults.withCredentials = true
 
 // interceptors
