77cd8cc3a2e9d839f8595e9f01a5616215d200c2
EcChris <42623282+EcChris@users.noreply.github.com>
2019-07-14 16:52:47 +0200

    Update ability.js

--- src/ability.js
+++ src/ability.js
@@ -1,12 +1,1 @@
-import { AbilityBuilder } from '@casl/ability'
 
-export default AbilityBuilder.define(can => {
-  //
-})
-
-/*
-const ability = new Ability([
-  { subject: 'all', actions: 'manage' },
-  { subject: 'Post', actions: 'delete', inverted: true }
-])
-*/
